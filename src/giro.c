5b06c63b4949baffa0b9428f7adf46d2863ac7ad
robertoacevedomora <roberto.acevedo@ucr.ac.cr>
2023-05-29 22:53:27 -0600

    Se modificaron algunos puertos como en el caso del spi_Setup porque el giroscopio no trabajaba de forma adecuada. Se agregaron las funciones y el codigo para que los datos del giroscopio aprezca en la pantalla

--- src/giro.c
+++ src/giro.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdint.h>//LCD-SERIAL
 #include <math.h>//LCD-SERIAL
+#include <string.h>
 
 #include "clock.h"
 #include "console.h"
@@ -44,29 +45,29 @@
 #define GYR_OUT_Z_H		0x2D
 
 //Faltan agregar ejes y,z.
-
+//Sensibilidad, importante
 #define L3GD20_SENSITIVITY_250DPS  (0.00875F) //Sensibilidad
 
 
 //Funcion para configurar la comunicacion spi, en este caso hay que usar SPI5. 
-//Utilizamos GPIOA y GPIOE. GPIO3, GPI05, GPIO6,GPIO7.
+//Utilizamos GPIOC y GPIOF. 
 static void spi_setup(void)
 {
 	rcc_periph_clock_enable(RCC_SPI5);
 	/* For spi signal pins */
-	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOC);
 	/* For spi mode select on the l3gd20 */
-	rcc_periph_clock_enable(RCC_GPIOE);
-
+	rcc_periph_clock_enable(RCC_GPIOF);
+//CAMBIO PUERTOS
 	/* Setup GPIOE3 pin for spi mode l3gd20 select. */
-	gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO3);
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
 	/* Start with spi communication disabled */
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
 	/* Setup GPIO pins for AF5 for SPI1 signals. */
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
-			GPIO5 | GPIO6 | GPIO7);
-	gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO6 | GPIO7);
+	gpio_mode_setup(GPIOF, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			GPIO7 | GPIO8 | GPIO9);
+	gpio_set_af(GPIOF, GPIO_AF5, GPIO7 | GPIO8 | GPIO9);
 
 	//spi initialization;
     //Comunicacion con el giroscopio.
@@ -87,27 +88,27 @@ static void spi_setup(void)
 }
 
 //Hasta aqui todo bien
-//Cambiamos aqui el purto A por el B para no generar confuciones.
+//Cambiamos aqui el purto B por el A para no generar confuciones, como estaba originalmente. Usamos USART1
 static void usart_setup(void)
 {
 	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
-	rcc_periph_clock_enable(RCC_USART2);
-	rcc_periph_clock_enable(RCC_GPIOB);
+	//rcc_periph_clock_enable(RCC_USART2);
 
 	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
-	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
-	gpio_set_af(GPIOB, GPIO_AF7, GPIO2| GPIO3);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
 
 	/* Setup UART parameters. */
-	usart_set_baudrate(USART2, 115200);
-	usart_set_databits(USART2, 8);
-	usart_set_stopbits(USART2, USART_STOPBITS_1);
-	usart_set_mode(USART2, USART_MODE_TX_RX);
-	usart_set_parity(USART2, USART_PARITY_NONE);
-	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
-
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	//usart_set_mode(USART2, USART_MODE_TX_RX);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+   //Usamos USART1, para ver si funciona.
 	/* Finally enable the USART. */
-	usart_enable(USART2);
+	usart_enable(USART1);
 }
 
 //Configurando el USART, puerto B pines 2 y 3.
@@ -115,12 +116,13 @@ static void usart_setup(void)
 //Funcion para configurar pines como entradas y salidas. Elejimos el puerto D.
 static void gpio_setup(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOD);
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
-		GPIO8 | GPIO9 | GPIO10 | GPIO11 | GPIO12 | GPIO13 |
-		GPIO14 | GPIO15);
-    gpio_mode_setup(GPIOD, GPIO_MODE_INPUT, GPIO_PUPD_NONE,
-		GPIO4 | GPIO5 ); //Para onexiones posteriores, pensando en USB, etc
+	rcc_periph_clock_enable(RCC_GPIOG); //PPP
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
+	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  //PPP
+		GPIO13);
+    gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+		GPIO14); //Para onexiones posteriores, pensando en USB, etc///PPP
 }
 
 
@@ -132,21 +134,21 @@ static void gpio_setup(void)
 
 static void giro_setup(void)
 {
-    gpio_clear(GPIOE, GPIO3);
+    gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_CTRL_REG1);
 	spi_read(SPI5);
 	spi_send(SPI5, GYR_CTRL_REG1_PD | GYR_CTRL_REG1_XEN |
 			GYR_CTRL_REG1_YEN | GYR_CTRL_REG1_ZEN |
 			(3 << GYR_CTRL_REG1_BW_SHIFT));
 	spi_read(SPI5);
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_CTRL_REG4);
 	spi_read(SPI5);
 	spi_send(SPI5, (1 << GYR_CTRL_REG4_FS_SHIFT));
 	spi_read(SPI5);
-	gpio_set(GPIOE, GPIO3);    
+	gpio_set(GPIOC, GPIO1);    
 }
 
 //Funcion que lee y retorna el valor de los 3 ejes
@@ -158,73 +160,73 @@ struct Giroscopio
 };
 typedef struct Giroscopio read_giro;
 
-read_giro Giro_ejes(void);
-read_giro Giro_ejes(void)
+read_giro leer_ejes(void);
+read_giro leer_ejes(void)
 {
     read_giro eje;
-
-    gpio_clear(GPIOE, GPIO3);
-	spi_send(SPI5, GYR_WHO_AM_I | GYR_RNW);
+//Parte explicada en la presentacion para enviar y recibir datos, hacerlo para cada uno de los ejes
+    gpio_clear(GPIOC, GPIO1);
+	spi_send(SPI5, GYR_WHO_AM_I | 0x80); //Leer el WHO I AM , or con 0x80
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_STATUS_REG | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_TEMP | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_X_L | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_x=spi_read(SPI5);    //En gyr_x se guarda el valor
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_X_H | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_x|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-    gpio_clear(GPIOE, GPIO3);
+    gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_Y_L | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_y=spi_read(SPI5);    //En gyr_x se guarda el valor
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_Y_H | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_y|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-    gpio_clear(GPIOE, GPIO3);
+    gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_Z_L | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_z=spi_read(SPI5);    //En gyr_x se guarda el valor
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
-	gpio_clear(GPIOE, GPIO3);
+	gpio_clear(GPIOC, GPIO1);
 	spi_send(SPI5, GYR_OUT_Z_H | GYR_RNW);
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_z|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
-	gpio_set(GPIOE, GPIO3);
+	gpio_set(GPIOC, GPIO1);
 
 	eje.gyr_x = eje.gyr_x*L3GD20_SENSITIVITY_250DPS;
 	eje.gyr_y = eje.gyr_y*L3GD20_SENSITIVITY_250DPS;
@@ -301,39 +303,45 @@ int main(void)
 	//Las dejamos porque se van a utilizar
     read_giro eje;
 	//Despues de la declaracion, inicializo en cero
-	//eje.gyr_x = 0;
-	//eje.gyr_y = 0;
-	//eje.gyr_z = 0;
+		eje.gyr_x = 0;
+	eje.gyr_y = 0;
+	eje.gyr_z = 0;
+		//Cadena de caracteres
+	char gyrp_x[10];
+	char gyrp_y[10];
+	char gyrp_z[10];
+
+	//Auxiliar
+	char aux[10] = "";
 	
+	
+	
+	
+	console_setup(115200);//LCD-SERIAL
+	clock_setup();//LCD-SERIAL
+	rcc_periph_clock_enable(RCC_USART1);    // Habilitacion del reloj del periferico USART1
+	rcc_periph_clock_enable(RCC_ADC1);     //Habilitacion adc, aun falta gregar estas dos partes bien.
 	gpio_setup();
-	usart_setup();
+	adc_setup();
+	sdram_init();
+	usart_setup(); //Funciones para el correcto funcionamiento del giroscopio.
+	
+	
 	spi_setup();
     giro_setup();
-    adc_setup();
-	clock_setup();//LCD-SERIAL
-	console_setup(115200);//LCD-SERIAL
-	sdram_init();
+	
+   
 	lcd_spi_init();
-	console_puts("LCD Initialized\n");
-	console_puts("Should have a checker pattern, press any key to proceed\n");
-	msleep(2000);
+	//console_puts("LCD Initialized\n");             //PPP
+	//console_puts("Should have a checker pattern, press any key to proceed\n"); //PPP
+	//msleep(2000);
 /*	(void) console_getc(1); */
 	gfx_init(lcd_draw_pixel, 240, 320);
 	//gfx_fillScreen(LCD_GREY);
 	
 	//Para realizar la lectura hay que formatear, de entero a cadena de caracteres.
 	//Creamos una cadena de caracteres
-	//Encabezados
-	char gyrpe_x[10];
-	char gyrpe_y[10];
-	char gyrpe_z[10];
-	
-	//Valores
-	char gyrp_x[10];
-	char gyrp_y[10];
-	char gyrp_z[10];
-
-	//Inicializar valores en cero
+	//Encabezado
 	
     
     // Comandos configuracion para el giroscopio, queda mejor como una nueva funcion porque el main lleva mas cosas.
@@ -358,43 +366,52 @@ int main(void)
 		//Formatear los valores de los ejes a cadenas de caracteres
 
 
-		sprintf(gyrpe_x, "%s", "Eje x:");       // Escribir el string "X:" en la cadena print_x
-		sprintf(gyrp_x, "%d",  eje.gyr_x);
-		sprintf(gyrpe_y, "%s", "Eje y:");       // Escribir el string "X:" en la cadena print_x
-		sprintf(gyrp_y, "%d",  eje.gyr_y);
-		sprintf(gyrpe_z, "%s", "Eje z:");       // Escribir el string "X:" en la cadena print_x
-		sprintf(gyrp_z, "%d",  eje.gyr_z);
+		sprintf(gyrp_x, "%s", "Eje x:");       
+		sprintf(aux, "%d",  eje.gyr_x);
+		strcat(gyrp_x ,aux); 
 
+		sprintf(gyrp_y, "%s", "Eje y:");       
+		sprintf(aux, "%d",  eje.gyr_y);
+		strcat(gyrp_y ,aux);
+
+		sprintf(gyrp_z, "%s", "Eje z:");       //Cadena, si se pone uno por uno se difuclta y la pantalla se traba.
+		sprintf(aux, "%d",  eje.gyr_z);
+		strcat(gyrp_z ,aux);
+		
+//Si se ponen muchas cosas no carga
 //Este blopque de codigo viene del archivo example, lcd-serial.c 
 		
 		gfx_fillScreen(LCD_CYAN); //Lena la pantalla principal
-		gfx_setCursor(15, 36);
-		gfx_setTextColor(LCD_BLUE, LCD_BLACK);
+		gfx_setCursor(15, 20);
+		gfx_setTextColor(LCD_BLUE, LCD_WHITE);
 		gfx_setTextSize(2);
-		gfx_puts("GIROSCOPIO");
-		
+		gfx_puts("Giroscopio");
+//Elegir colores que resalten
 	    gfx_setCursor(15,70);
-		gfx_setTextSize(1);
-		gfx_setTextColor(LCD_BLACK,LCD_GREEN);
+		gfx_setTextSize(2);
+		gfx_setTextColor(LCD_BLUE,LCD_WHITE);
 		gfx_puts(gyrp_x);
-
+//setCursor, posicion en pantalla donde colocamos los datos
+//TextSize, tamano de las letras. Probamos con 3 y 1, pero 2 se ve bien
+//puts, agregamos la cadena de caracteres despues de cambiar el formato del valor de los ejes
 		gfx_setCursor(15,100);
-		gfx_setTextSize(1);
-		gfx_setTextColor(LCD_BLACK,LCD_GREEN);
+		gfx_setTextSize(2);
+		gfx_setTextColor(LCD_BLUE,LCD_WHITE);
 		gfx_puts(gyrp_y);
 
-		gfx_setCursor(15,130);
-		gfx_setTextSize(1);
-		gfx_setTextColor(LCD_BLACK,LCD_GREEN);
-		gfx_puts(gyrp_z);
 
-		gfx_setCursor(15,190);
+		gfx_setCursor(15,130);
 		gfx_setTextSize(2);
-		gfx_setTextColor(LCD_BLACK,LCD_GREEN);
-		gfx_puts(gyrpe_x);
+		gfx_setTextColor(LCD_BLUE,LCD_WHITE);
+		gfx_puts(gyrp_z);
 		
 		//gfx_fillCircle(120, 160, 40, LCD_YELLOW);
 		lcd_show_frame();
+		gpio_clear(GPIOC, GPIO1); 
+		eje = leer_ejes(); //Leer el valor de los ejes
+		gpio_set(GPIOC, GPIO1); 
+
+		
 
 //Esto permite lectura de registros y del eje x, hay que agregarle la parte alta del eje x y los ejes z y y.
 //Eliminamos los 8, cambiamos SPI1 por SPI5. Tambien segun la rutina de las diapositvas falta agregarle un read, que
