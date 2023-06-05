d20f3487b213029a580be102b97d924368df3588
robertoacevedomora <roberto.acevedo@ucr.ac.cr>
2023-06-04 19:15:40 -0600

    Comentando el codigo y quitando comentarios previos

--- src/MqttPub.py
+++ src/MqttPub.py
@@ -4,18 +4,10 @@ import json
 import time
 import os
 from datetime import datetime
-#import ssl, socket
 
-#Ya con esto transmito los datos
-#Puerto
 ser = serial.Serial('COM4',115200,timeout =0)
 
-#Broker
 
-#broker = "iot.eie.ucr.ac.cr"
-
-#topic= "vI/devices/me/telemetry" 
-#username = "holadevice"
 ACCESS_TOKEN = 'JAdeAfZaduFGAbQJxtTx'
 broker = 'demo.thingsboard.io'
 port = 1883
@@ -23,7 +15,7 @@ port = 1883
 def on_publish(client, userdata, result):
     print("data published to thingsboard /n")
     pass
-client1 = mqtt.Client("Roberto")
+client1 = mqtt.Client("Giroscopio")
 client1.on_publish = on_publish
 client1.username_pw_set(ACCESS_TOKEN)
 client1.connect(broker,port,keepalive=60)
@@ -36,13 +28,11 @@ while (1):
     dict["X"] = ejes[0]
     dict["Y"] = ejes[1]
     dict["Z"] = ejes[2]
-    #x,y y z quedan divididos
-    #json_data = {"Eje x": float(x), "Eje y":float(y), "Eje z":float(z)}
-    #client1.publish("vI/devices/me/telemetry",json.dumps(json_data))
+    dict["Nivel"] = ejes[3]
     hola = json.dumps(dict)
     client1.publish("v1/devices/me/telemetry",hola)
     print(hola)
-    time.sleep(5)
+    time.sleep(1)
     
 
 
--- src/giro.c
+++ src/giro.c
@@ -2,161 +2,152 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdint.h>//LCD-SERIAL
-#include <math.h>//LCD-SERIAL
+#include <stdint.h>    //Del archivo lcd-spi.h
+#include <math.h>     //Del archivo lcd-spi.h
 #include <string.h>
 
-#include "clock.h"
-#include "console.h"
-#include "sdram.h"
-#include "lcd-spi.h"
+#include "clock.h"   //Del archivo lcd-spi.h
+#include "console.h" //Del archivo lcd-spi.h
+#include "sdram.h"   //Del archivo lcd-spi.h   
+#include "lcd-spi.h" //Del archivo lcd-spi.h
 #include "gfx.h"
 
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/dac.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/usart.h>
-#include <libopencm3/stm32/spi.h>
-#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>   //Del archivo spi.c, examples f3
+#include <libopencm3/stm32/usart.h> //Del archivo spi.c, examples f3
+#include <libopencm3/stm32/spi.h>   //Del archivo spi.c, examples f3
+#include <libopencm3/stm32/gpio.h>  //Del archivo spi.c, examples f3
 
 
 
 
-//Giroscopio
+//Del archivo spi.c, examples, stm32, f3. Para utilizar el giroscopio 
 #define GYR_RNW			(1 << 7) /* Write when zero */
 #define GYR_MNS			(1 << 6) /* Multiple reads when 1 */
-#define GYR_WHO_AM_I		0x0F
-#define GYR_OUT_TEMP		0x26
-#define GYR_STATUS_REG		0x27
-#define GYR_CTRL_REG1		0x20
-#define GYR_CTRL_REG1_PD	(1 << 3)
-#define GYR_CTRL_REG1_XEN	(1 << 1)
-#define GYR_CTRL_REG1_YEN	(1 << 0)
-#define GYR_CTRL_REG1_ZEN	(1 << 2)
-#define GYR_CTRL_REG1_BW_SHIFT	4
-#define GYR_CTRL_REG4		0x23
-#define GYR_CTRL_REG4_FS_SHIFT	4
+#define GYR_WHO_AM_I		0x0F      //Registro identificacion
+#define GYR_OUT_TEMP		0x26      //Registro temperatura
+#define GYR_STATUS_REG		0x27      //Registro estado
+#define GYR_CTRL_REG1		0x20      //Registro control
+#define GYR_CTRL_REG1_PD	(1 << 3)  //Registro power_down
+#define GYR_CTRL_REG1_XEN	(1 << 1)  //Habilito eje x
+#define GYR_CTRL_REG1_YEN	(1 << 0)  //Habilito eje y
+#define GYR_CTRL_REG1_ZEN	(1 << 2)  //Habilito eje z
+#define GYR_CTRL_REG1_BW_SHIFT	4     //Ancho de banda
+#define GYR_CTRL_REG4		0x23      //SPI serial interface mode selection.
+#define GYR_CTRL_REG4_FS_SHIFT	4     //Full scale selection
 
-#define GYR_OUT_X_L		0x28
-#define GYR_OUT_X_H		0x29
-#define GYR_OUT_Y_L		0x2A
-#define GYR_OUT_Y_H		0x2B
-#define GYR_OUT_Z_L		0x2C
-#define GYR_OUT_Z_H		0x2D
-
-//Faltan agregar ejes y,z.
-//Sensibilidad, importante
-#define Sensibilidad (0.00875F) //Sensibilidad
+#define GYR_OUT_X_L		0x28 //Direccion eje x. X-axis angular rate data. The value is expressed as two’s complement.
+#define GYR_OUT_X_H		0x29 //Direccion eje x
+#define GYR_OUT_Y_L		0x2A //Direccion eje y. Y-axis angular rate data. The value is expressed as two’s complement.
+#define GYR_OUT_Y_H		0x2B //Direccion eje y
+#define GYR_OUT_Z_L		0x2C //Direccion eje z. Z-axis angular rate data. The value is expressed as two’s complement.
+#define GYR_OUT_Z_H		0x2D //Direccion eje z
 
 
-//Funcion para configurar la comunicacion spi, en este caso hay que usar SPI5. 
-//Utilizamos GPIOC y GPIOF. 
+#define Sensibilidad  (0.00875F) //Sensibilidad, caracteristica mecanica 8.75
+
+
+//Funcion para configurar la comunicacion SPI.
+//Funcion extraida del archivo spi.c, de los ejemplos. 
+//Los puertos se elijieron segun el archivo f4, spi-mems.c
 static void spi_setup(void)
 {
-	rcc_periph_clock_enable(RCC_SPI5);
-	/* For spi signal pins */
-	rcc_periph_clock_enable(RCC_GPIOC);
-	/* For spi mode select on the l3gd20 */
-	rcc_periph_clock_enable(RCC_GPIOF);
-//CAMBIO PUERTOS
-	/* Setup GPIOE3 pin for spi mode l3gd20 select. */
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
-	/* Start with spi communication disabled */
-	gpio_set(GPIOC, GPIO1);
+	//Se cambiaron los puertos A -> C y E -> F.
+	//Se cambio el SPI, SPI1 -> SPI5.
+	rcc_periph_clock_enable(RCC_SPI5);  //Habilito reloj SPI5.
+	rcc_periph_clock_enable(RCC_GPIOC); //Habilito reloj Puerto C
+	rcc_periph_clock_enable(RCC_GPIOF); //Hbailito reloj Puerto F
 
-	/* Setup GPIO pins for AF5 for SPI1 signals. */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1); //Pin 1, puerto C como salida
+/* Start with spi communication disabled */
+	gpio_set(GPIOC, GPIO1);
+//Pin 7, 8 y 9 del puerto F funciones SPI.
 	gpio_mode_setup(GPIOF, GPIO_MODE_AF, GPIO_PUPD_NONE,
-			GPIO7 | GPIO8 | GPIO9);
+			GPIO7 | GPIO8 | GPIO9);                       
 	gpio_set_af(GPIOF, GPIO_AF5, GPIO7 | GPIO8 | GPIO9);
 
-	//spi initialization;
+	//SPI initialization
+	//Inicializar y configurar el protocolo SPI
     //Comunicacion con el giroscopio.
 	spi_set_master_mode(SPI5);
 	spi_set_baudrate_prescaler(SPI5, SPI_CR1_BR_FPCLK_DIV_64);
-	spi_set_clock_polarity_0(SPI5);
-	spi_set_clock_phase_0(SPI5);
-	spi_set_full_duplex_mode(SPI5);
-	spi_set_unidirectional_mode(SPI5); /* bidirectional but in 3-wire */
-	//spi_set_data_size(SPI5, SPI_CR1_DS_8BIT);
+	spi_set_clock_polarity_0(SPI5); //Polaridad del reloj
+	spi_set_clock_phase_0(SPI5);    //Fase del reloj
+	spi_set_full_duplex_mode(SPI5);  //Comuniacion duplex
+	spi_set_unidirectional_mode(SPI5);  
 	spi_enable_software_slave_management(SPI5);
 	spi_send_msb_first(SPI5);
 	spi_set_nss_high(SPI5);
-	//spi_enable_ss_output(SPI1);
-	//spi_fifo_reception_threshold_8bit(SPI5);
 	SPI_I2SCFGR(SPI5) &= ~SPI_I2SCFGR_I2SMOD;
-	spi_enable(SPI5);
+	spi_enable(SPI5); //Habilita el protrocolo SPI
 }
 
-//Hasta aqui todo bien
-//Cambiamos aqui el purto B por el A para no generar confuciones, como estaba originalmente. Usamos USART1
+
+//Funcion extraida del archivo spi.c, de los ejemplos. 
 static void usart_setup(void)
 {
-	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART2. */
-	//rcc_periph_clock_enable(RCC_USART2);
-
+	
 	/* Setup GPIO pin GPIO_USART2_TX/GPIO9 on GPIO port A for transmit. */
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
 	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
 
-	/* Setup UART parameters. */
-	usart_set_baudrate(USART1, 115200); //Python
-	usart_set_databits(USART1, 8);
-	usart_set_stopbits(USART1, USART_STOPBITS_1);
-	//usart_set_mode(USART2, USART_MODE_TX_RX);
-	usart_set_mode(USART1, USART_MODE_TX);
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-   //Usamos USART1, para ver si funciona.
-	/* Finally enable the USART. */
-	usart_enable(USART1);
+	//Configuramos los parametros para el USART
+	usart_set_baudrate(USART1, 115200); //Velocidad
+	usart_set_databits(USART1, 8);     //Bits
+	usart_set_stopbits(USART1, USART_STOPBITS_1); //Paradas
+	usart_set_mode(USART1, USART_MODE_TX);        //Configuro USART1 para la transmision
+	usart_set_parity(USART1, USART_PARITY_NONE); //Elimino bits de paridad
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE); //Elimino control de flujo
+	usart_enable(USART1);                                     //Habilito USART 1
 }
 
 //Configurando el USART, puerto B pines 2 y 3.
 
-//Funcion para configurar pines como entradas y salidas. Elejimos el puerto D.
-static void gpio_setup(void)
+static void led_con_setup(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOG); //PPP
-	rcc_periph_clock_enable(RCC_GPIOA);
-	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
-	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  //PPP
+	rcc_periph_clock_enable(RCC_GPIOG); //Habilitamos reloj del puerto G
+	rcc_periph_clock_enable(RCC_GPIOA);  //Habilitamos reloj del puerto A
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);  //Puerto A, pin 0 como entrada. Boton
+	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, //Puerto G, pin 13 como salida. LED
 		GPIO13);
     gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
-		GPIO14); //Para onexiones posteriores, pensando en USB, etc///PPP
+		GPIO14); //Puerto G, pin 14 como salida. LED
 }
 
 
 //GPIO configurados
 //Eliminamos de momento esta funcion, no hace falta (my_usart_print_int)
 
-//De momento dejamos clock_setup
-
-
+//giro_setup, tomamos como referencia el ejemplo spi.c, especificamente la funcion main
+//Control ejes y mode de poder
 static void giro_setup(void)
 {
-    gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_CTRL_REG1);
-	spi_read(SPI5);
+    gpio_clear(GPIOC, GPIO1); //Modificamos el chip select para poner a funcionar el giroscopio.
+	spi_send(SPI5, GYR_CTRL_REG1); //Enviamos registro
+	spi_read(SPI5); //Leemos la respuesta
 	spi_send(SPI5, GYR_CTRL_REG1_PD | GYR_CTRL_REG1_XEN |
 			GYR_CTRL_REG1_YEN | GYR_CTRL_REG1_ZEN |
-			(3 << GYR_CTRL_REG1_BW_SHIFT));
-	spi_read(SPI5);
-	gpio_set(GPIOC, GPIO1);
-
-	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_CTRL_REG4);
-	spi_read(SPI5);
-	spi_send(SPI5, (1 << GYR_CTRL_REG4_FS_SHIFT));
-	spi_read(SPI5);
-	gpio_set(GPIOC, GPIO1);    
+			(3 << GYR_CTRL_REG1_BW_SHIFT)); //Enviamos registros de control eje x, y y z
+	spi_read(SPI5); //Leemos la respuesta
+	gpio_set(GPIOC, GPIO1); // Modificamos otra vez el chip select para poner detener el funcionamiento del giroscopio.
+    
+	//Configuracion dps y modo spi
+	gpio_clear(GPIOC, GPIO1); //Modificamos el chip select para poner a funcionar el giroscopio.
+	spi_send(SPI5, GYR_CTRL_REG4); //Enviamos el registro
+	spi_read(SPI5); //Lectura de la respuesta
+	spi_send(SPI5, (1 << GYR_CTRL_REG4_FS_SHIFT)); //Enviamos escala de medicion
+	spi_read(SPI5); //Lectura de la respuesta
+	gpio_set(GPIOC, GPIO1);  // Modificamos otra vez el chip select para poner detener el funcionamiento del giroscopio.  
 }
 
+//Estructura Giroscopio, con los 3 ejes
 //Funcion que lee y retorna el valor de los 3 ejes
 struct Giroscopio 
 {
-    int16_t gyr_x;
-    int16_t gyr_y;
-    int16_t gyr_z;
+    int16_t gyr_x; //Variable de 16 bits, para guardar parte alta y baja del eje x.
+    int16_t gyr_y; //Variable de 16 bits, para guardar parte alta y baja del eje y. 
+    int16_t gyr_z; //Variable de 16 bits, para guardar parte alta y baja del eje z. 
 };
 typedef struct Giroscopio read_giro;
 
@@ -166,93 +157,92 @@ read_giro leer_ejes(void)
     read_giro eje;
 //Parte explicada en la presentacion para enviar y recibir datos, hacerlo para cada uno de los ejes
     gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_WHO_AM_I | 0x80); //Leer el WHO I AM , or con 0x80
+	spi_send(SPI5, GYR_WHO_AM_I | 0x80); //Leer el WHO I AM , or con 0x80. Registro de identificación del dispositivo
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_STATUS_REG | GYR_RNW);
+	spi_send(SPI5, GYR_STATUS_REG | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_TEMP | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_TEMP | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	spi_read(SPI5);
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_X_L | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_X_L | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_x=spi_read(SPI5);    //En gyr_x se guarda el valor
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_X_H | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_X_H | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_x|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
 	gpio_set(GPIOC, GPIO1);
 
     gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_Y_L | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_Y_L | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_y=spi_read(SPI5);    //En gyr_x se guarda el valor
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_Y_H | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_Y_H | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_y|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
 	gpio_set(GPIOC, GPIO1);
 
     gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_Z_L | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_Z_L | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_z=spi_read(SPI5);    //En gyr_x se guarda el valor
 	gpio_set(GPIOC, GPIO1);
 
 	gpio_clear(GPIOC, GPIO1);
-	spi_send(SPI5, GYR_OUT_Z_H | GYR_RNW);
+	spi_send(SPI5, GYR_OUT_Z_H | GYR_RNW); //Registro de  lectura
 	spi_read(SPI5);
 	spi_send(SPI5, 0);
 	eje.gyr_z|=spi_read(SPI5) << 8;  //Desplazamiento y mascar or
 	gpio_set(GPIOC, GPIO1);
 
-	eje.gyr_x = eje.gyr_x*Sensibilidad;
-	eje.gyr_y = eje.gyr_y*Sensibilidad;
-	eje.gyr_z = eje.gyr_z*Sensibilidad;
+	eje.gyr_x = eje.gyr_x*Sensibilidad; //Sensibilidad del giroscopio
+	eje.gyr_y = eje.gyr_y*Sensibilidad; //Sensibilidad del giroscopio
+	eje.gyr_z = eje.gyr_z*Sensibilidad; //Sensibilidad del giroscopio
 
     return eje;
 
 }
 
 /***********Convertidor analogico digital, funciones del archivo ejemplo********************/
-//Congiura el convertidor analogico digital
+//Configura el convertidor analogico digital, referencias del ejemplo adc-dac-printf.c
 static void adc_setup(void)
 {
-	//Prints the ADC value on PA0 (adc channel 0) on the console, eliminamo el A1
-   // gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0); //Revisarlo, solo usamos uno adc
-	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
+	//Imprime el valor del convertidor analogico digital en el pin 1 del puerto A.
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);//Configuramos el pin 1 del puerto A como entrada.
 
-	adc_power_off(ADC1);
-	adc_disable_scan_mode(ADC1);
-	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_3CYC);
+	adc_power_off(ADC1);      //Apagamos el convertidor
+	adc_disable_scan_mode(ADC1); //Quitamos el scan mode
+	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_3CYC); //Muestreamos
 
-	adc_power_on(ADC1);
+	adc_power_on(ADC1); //Encendemos
 
 }
 
-//Funcion para leer el convertidor analogico digital
+//Funcion para leer el valor analogico de ADC1, ejemplo adc-dac-printf.
 static uint16_t read_adc_naiive(uint8_t channel)
 {
 	uint8_t channel_array[16];
@@ -274,7 +264,7 @@ int print_decimal(int num)
 	int		len = 0;
 	char	is_signed = 0;
 
-	if (num < 0) {
+	if (num < 0) { //Revisa si el numero es negativo o positivo
 		is_signed++;
 		num = 0 - num;
 	}
@@ -297,55 +287,46 @@ int print_decimal(int num)
 
 int main(void)
 {
-	//uint8_t temp;
-	//int16_t gyr_x; //Variable de 16 bits que guarda el valor de gyr_x. Hacer otras dos para y y z.
 	
-	//Las dejamos porque se van a utilizar
     read_giro eje;
 	//Despues de la declaracion, inicializo en cero
 	eje.gyr_x = 0;
 	eje.gyr_y = 0;
-	eje.gyr_z = 0;
-		//Cadena de caracteres
+	eje.gyr_z = 0;	
+	//Cadena de caracteres para agregar el valor de los ejes y mensaje
 	char gyrp_x[10];
 	char gyrp_y[10];
 	char gyrp_z[10];
-	
-
-	//Conexion
-
+	//Conexion USART
 	char * USB_cone = "OFF"; 
 	char cone[10];
-	 bool conexion = false; 
-	//Bateria
+	bool conexion = false; 
+	
+	//Nivel de la bateria
 	float nivel; //Variable nivel de bateria
-	char nivel_p[10];			// Variable para indicar si se envian los datos
-	uint16_t adc1;              //Entero de la funcion adc
+	nivel = 0;
+	char nivel_p[10];		
+	uint16_t ana_dig;              //Entero de la funcion adc
 
 	//Auxiliar
 	char aux[10] = "";
 	
+	
+	//Declaracion de funciones 
+
 	console_setup(115200);//LCD-SERIAL
 	clock_setup();//LCD-SERIAL
 	rcc_periph_clock_enable(RCC_USART1);    // Habilitacion del reloj del periferico USART1
 	rcc_periph_clock_enable(RCC_ADC1);     //Habilitacion adc, aun falta gregar estas dos partes bien.
-	gpio_setup();
+	led_con_setup();
 	adc_setup();
 	sdram_init();
 	usart_setup(); //Funciones para el correcto funcionamiento del giroscopio.
-	
-	
 	spi_setup();
     giro_setup();
-	
-   
 	lcd_spi_init();
-	//console_puts("LCD Initialized\n");             //PPP
-	//console_puts("Should have a checker pattern, press any key to proceed\n"); //PPP
-	//msleep(2000);
-/*	(void) console_getc(1); */
 	gfx_init(lcd_draw_pixel, 240, 320);
-	//gfx_fillScreen(LCD_GREY);
+
 	
 
 
@@ -365,7 +346,9 @@ int main(void)
 		sprintf(gyrp_z, "%s", "Eje z:");       //Cadena, si se pone uno por uno se difuclta y la pantalla se traba.
 		sprintf(aux, "%d",  eje.gyr_z);
 		strcat(gyrp_z ,aux);
-		
+
+//Mensajes pantalla LCD ejes
+
 //Si se ponen muchas cosas no carga
 //Este blopque de codigo viene del archivo example, lcd-serial.c 
 		
@@ -392,14 +375,14 @@ int main(void)
 		gfx_setTextSize(2);
 		gfx_setTextColor(LCD_BLUE,LCD_WHITE);
 		gfx_puts(gyrp_z);
- //Imprimir nivel de la bateria
-		adc1 = read_adc_naiive(1);       
-		// nivel = ((adc1*8.64)/510); 
-		nivel = adc1;
 
+
+ //Mensaje pantalla nivel bateria       
+	
 	    sprintf(nivel_p, "%s", "Nivel:");
 		sprintf(aux, "%f",  nivel);
 		strcat(nivel_p, aux);
+	
 
 		gfx_setCursor(15,160);
 		gfx_setTextSize(2);
@@ -408,7 +391,7 @@ int main(void)
 
 
 
- //Imprimir USB
+ //Mensaje pantalla LCD conexion
 
 		sprintf(cone, "%s", "Conexion:");
 		sprintf(aux, "%s",  USB_cone);
@@ -422,57 +405,59 @@ int main(void)
 
 
 
-		//gfx_fillCircle(120, 160, 40, LCD_YELLOW);
+		//Funciones pantalla
 		lcd_show_frame();
 		gpio_clear(GPIOC, GPIO1); 
 		eje = leer_ejes(); //Leer el valor de los ejes
 		gpio_set(GPIOC, GPIO1); 
-
-
-   //Bateria y adc
-		
-
+        ana_dig = read_adc_naiive(1);
+		nivel = ((ana_dig*9)/4095);
 		
 
 //Esto permite lectura de registros y del eje x, hay que agregarle la parte alta del eje x y los ejes z y y.
 //Eliminamos los 8, cambiamos SPI1 por SPI5. Tambien segun la rutina de las diapositvas falta agregarle un read, que
 //en este caso aprece con un temp.		
 
-		if (conexion)   //USART                             
-		{
-			USB_cone = "Encendida";            // Enviamos los valores de los ejes
+		if (conexion)                               
+		{ 
+			USB_cone = "ON";            // Indica si puerto funciona
 			print_decimal(eje.gyr_x);             
 			console_puts("\t");
        	 	print_decimal(eje.gyr_y);
 			console_puts("\t");
         	print_decimal(eje.gyr_z); 
 			console_puts("\t");
-			
+			print_decimal(nivel); 
+			USB_cone = "ON"; 
+			console_puts("\n");
+			// Toggle del pin 13, puerto G, parpadeo indica envio exitoso 
 			gpio_toggle(GPIOG, GPIO13);     
 		}
+		
 		else{                                     
-			USB_cone = "Apagada";     //Chequeo de leds     
-			
+			USB_cone = "OFF";        
+			//Conexion OFF, no parpadea y se apaga
 			gpio_clear(GPIOG, GPIO13);             
 		}
 
 		
 		if (nivel<7)
 		{   
-			
+			// Pin 14 parpadea, indica bateria baja nivel menor a 7
 			gpio_toggle(GPIOG, GPIO14);
 		}
 
-		else gpio_clear(GPIOG, GPIO14); 
+		else gpio_clear(GPIOG, GPIO14); //Bateria alta, apaga el led pin 14 puerto G
 		
 
-		
+		//Revisa si el boton esta en alto o bajo
 		if (gpio_get(GPIOA, GPIO0)) {      
 			
 			if (conexion) {
-				conexion = false;
-				gpio_clear(GPIOG, GPIO13);
+				conexion = false; //No hay conexion
+				gpio_clear(GPIOG, GPIO13); 
 			}
+			
 			else conexion = true;
 		}
 //Para el ciclo
